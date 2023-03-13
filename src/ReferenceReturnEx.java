31ba4e11774327f51d27b4ca5400e8e7de57d3ff
hoonieee333 <hoonieee333@gmail.com>
2023-03-13 20:06:56 +0900

    Java

--- src/ReferenceReturnEx.java
+++ src/ReferenceReturnEx.java
@@ -1,18 +1,16 @@
-class Data { int x; }
+class DataD { int x; }
+
 public class ReferenceReturnEx {
   public static void main(String[] args) {
-    Data d1 = new Data();
-    d1.x = 10;
+    DataD d = new DataD();
+    d.x = 10;
 
-    Data d2 = copy(d1);
-
-    System.out.println(d1.x);
+    DataD d2 = copy(d);
     System.out.println(d2.x);
-
   }
-  static Data copy(Data argRef) {
-    Data tmp = new Data();
-    tmp.x = argRef.x;
+  static DataD copy(DataD argAddr) {
+    DataD tmp = new DataD();
+    tmp.x = argAddr.x;
     return tmp;
   }
 }
